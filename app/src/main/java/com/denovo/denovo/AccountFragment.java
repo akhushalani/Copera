262138ebfa60cd5fc857bb6f46748a35739df565
nickcowens17 <nickcowens17@gmail.com>
2017-01-09 11:02:32 -0500

    Addded Account Fragment Layout

--- app/src/main/java/com/denovo/denovo/AccountFragment.java
+++ app/src/main/java/com/denovo/denovo/AccountFragment.java
@@ -1,14 +1,26 @@
 package com.denovo.denovo;
 
+import android.app.Activity;
+import android.content.ActivityNotFoundException;
 import android.content.Context;
+import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.Toast;
 
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
+import static java.security.AccessController.getContext;
 
 public class AccountFragment extends Fragment {
 
@@ -22,6 +34,5 @@ public class AccountFragment extends Fragment {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_account, container, false);
     }
-
 
 }
