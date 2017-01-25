7e61301c659f66bc05a8eed6af53e778907df0f0
nickcowens17 <nickcowens17@gmail.com>
2017-01-25 11:48:10 -0500

    Updated account fragment

--- app/src/main/java/com/denovo/denovo/AccountFragment.java
+++ app/src/main/java/com/denovo/denovo/AccountFragment.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,12 +12,24 @@ import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.ArrayList;
 
 
 public class AccountFragment extends Fragment {
 
     private String name;
-    private CustomButton createChapterButton;
+    private ArrayList<String> wishlist;
+    private RVAdapter mAdapter;
+    private WrapContentLinearLayoutManager llm;
+
+    private DatabaseReference mDatabase;
+
 
     public AccountFragment() {
         // Required empty public constructor
@@ -27,6 +40,10 @@ public class AccountFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View rootView = inflater.inflate(R.layout.fragment_account, container, false);
+        RecyclerView rv = (RecyclerView) rootView.findViewById(R.id.wishlist_rv);
+        llm = new WrapContentLinearLayoutManager(getContext());
+        rv.setLayoutManager(llm);
+        wishlist = new ArrayList<>();
 
         FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
         if (user != null) {
@@ -36,7 +53,12 @@ public class AccountFragment extends Fragment {
         TextView userNameTextView = (TextView) rootView.findViewById(R.id.user_name_text_view);
         userNameTextView.setText(name);
 
+        mDatabase = FirebaseDatabase.getInstance().getReference();
+
+
+
         return rootView;
     }
+
 
 }
